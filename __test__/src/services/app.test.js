024fc0b5ca807bb4dddd126cce5207f485fa18db
italoalmeida <it4lo.almeida@gmail.com>
2021-03-18 19:40:14 -0300

    REFACTOR: config typescript - initialSetup

--- __test__/src/services/app.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest')
-const app = require('../../../src/ecma5');
-
-test('#Should listen', () => {
-    return request(app).get('/')
-        .then((res) => {
-            expect(res.status).toBe(200)
-        })
-})
